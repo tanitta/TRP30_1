72b8acd904f40942282fc4c62384907dde649df7
tanitta <ttata.trit@gmail.com>
2014-09-05 14:07:25 +0900

    Add Core.lua

---
+++ Core.lua
@@ -0,0 +1,17 @@
+namespace "trit"{
+	namespace "model"{
+		namespace "trp30_1"{
+			class "Core"{
+				metamethod"_init"
+				:body(function(self)
+				end);
+
+				metamethod"__call"
+				:body(function(self)
+				end);
+
+
+			}
+		}--trp30_1
+	}--model
+}--trit
